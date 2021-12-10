6d1ab52f62aa0912f9beedde82b5d735e8f4fcaf
TareqMonwer <tareqmonwer137@gmail.com>
2021-12-10 11:18:05 +0600

    ch2: templates.

---
+++ README.md
@@ -0,0 +1,5 @@
+### Setup:
+After setting up the virtual-environment, run these commands in your terminal:
++ `pip install -r requirements.txt`
++ `export FLASK_APP=microblog.py`
++ `flask run`
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,11 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello world!"
+    context = {
+        'teitle': 'Interactive Learning'
+    }
+    return render_template('index.html', **context)
