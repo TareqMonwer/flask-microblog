ea1c2fc650ea3fc0816409a9f26ec6bb3002de9d
TareqMonwer <tareqmonwer137@gmail.com>
2021-12-10 17:25:53 +0600

    ch3: creating forms with wt-forms.

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 
 @app.route('/')
@@ -9,3 +10,12 @@ def index():
         'teitle': 'Interactive Learning'
     }
     return render_template('index.html', **context)
+
+
+@app.route('/login')
+def login():
+    context = {
+        'form': LoginForm(),
+        'title': 'Sign In'
+    }
+    return render_template('login.html', **context)
