98e1236384549105757b27ae5ca8d5624a947040
TareqMonwer <tareqmonwer137@gmail.com>
2021-12-25 01:56:29 +0600

    :zap: add login logic.

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
-from flask import render_template, flash, redirect
+from flask_login import current_user, login_user, logout_user
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
+from app.models import User
 
 
 @app.route('/')
@@ -14,10 +16,16 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        username, remember_me = form.username.data, form.remember_me.data
-        flash(f'Login requested for user {username}, remember_me={remember_me}')
+        username, remember_me, password = form.username.data, form.remember_me.data, form.password.data,
+        user = User.query.filter_by(username=username).first()
+        if user is None or not user.check_password(password):
+            flash('Invalid credentials!')
+            return redirect(url_for('login'))
+        login_user(user, remember_me)
         return redirect('/index')
 
     context = {
@@ -25,3 +33,9 @@ def login():
         'title': 'Sign In'
     }
     return render_template('login.html', **context)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
