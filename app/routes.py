bdc8ad845c6d1345513b284d49ea05c4fdd800cc
TareqMonwer <tareqmonwer137@gmail.com>
2021-12-25 02:28:49 +0600

    :zap: complete registration process.

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,8 @@
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 from flask import render_template, flash, redirect, url_for, request
-from app import app
-from app.forms import LoginForm
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
 from app.models import User
 
 
@@ -15,6 +15,22 @@ def index():
         'posts': current_user.posts
     }
     return render_template('index.html', **context)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    context = {'title': 'Create Account', 'form': form}
+    return render_template('registration.html', **context)
 
 
 @app.route('/login', methods=['GET', 'POST'])
