76c0e2d99d4449d0af65bcb369a3f58288ec89c1
TareqMonwer <tareqmonwer137@gmail.com>
2021-12-25 13:58:18 +0600

    :zap: profile system init finish.

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from wtforms import StringField, BooleanField, TextAreaField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, Length
 from app.models import User
 
 
@@ -28,3 +28,10 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError('Please, use a different email!')
+
+
+class ProfileEditForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), ])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    about_me = TextAreaField('About me', validators=[DataRequired(), Length(min=0, max=150)])
+    submit = SubmitField('Submit')
--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,7 @@ from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 from flask import render_template, flash, redirect, url_for, request
 from app import app, db
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, ProfileEditForm
 from app.models import User
 
 
@@ -89,3 +89,21 @@ def user_profile(username):
     ]
     context = {'user': user, 'posts': posts}
     return render_template('user_profile.html', **context)
+
+
+@app.route('/u/edit/', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = ProfileEditForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile', form=form)
